38f6f74a12ba47f1154409aff6a70ce3cea8ac90
YashMowdhgalya <yashwanthavmowdhgalya3@gmail.com>
2023-01-20 10:50:35 +0530

    restructuring

--- Step 1- Learn the basics/Step 1.4 - Know basic maths/maths.cpp
+++ Step 1- Learn the basics/Step 1.4 - Know basic maths/maths.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
+#include<string.h>
+#include<math.h>
 using namespace std;
 void digits(int number)
 {
+    // using conventional divide by 10 method - O(n) where n is number of digits
     int digits;
     while(number > 0)
     {
@@ -9,6 +12,16 @@ void digits(int number)
         digits++;
     }
     cout << "Number of digits are " << digits;
+
+    /* method 2 - convert number to string and find the length of the string - O(1)
+        string num = to_string(number);
+        cout << num.length();
+    */
+
+    /* method 3 - use log base 10 method - O(1)
+        cout << floor(log10(number)+1);
+    */
+
 }
 int main()
 {
